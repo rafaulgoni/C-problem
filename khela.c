55cc2dbfa240eb660ba058ab3ef63413c45be081
unknown <rafaulgoni787898@gmail.com>
2024-10-22 00:32:53 +0600

    Palindrome array

--- khela.c
+++ khela.c
@@ -739,8 +739,25 @@
 // }
 
 
-#include <stdio.h>
+// #include <stdio.h>
 
+// int main() {
+//     int N;
+//     scanf("%d", &N);
+    
+//     int A[N];
+//     for (int i = 0; i < N; i++) {
+//         scanf("%d", &A[i]);
+//     }
+    
+//     for (int i = N - 1; i >= 0; i--) {
+//         printf("%d ", A[i]);
+//     }
+    
+//     return 0;
+// }
+
+#include <stdio.h>
 int main() {
     int N;
     scanf("%d", &N);
@@ -749,11 +766,21 @@ int main() {
     for (int i = 0; i < N; i++) {
         scanf("%d", &A[i]);
     }
+
+    int isPalindrome = 1;  
     
-    for (int i = N - 1; i >= 0; i--) {
-        printf("%d ", A[i]);
+    for (int i = 0; i < N / 2; i++) {
+        if (A[i] != A[N - i - 1]) {
+            isPalindrome = 0; 
+            break;
+        }
+    }
+    
+    if (isPalindrome) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
     }
     
     return 0;
 }
-
